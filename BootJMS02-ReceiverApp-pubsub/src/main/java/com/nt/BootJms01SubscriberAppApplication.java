3db51f1757351d23c30ef6d02929085f0274b9f2
HarishAnagoni <harish.anagoni@gmail.com>
2025-05-26 21:56:28 +0530

    team leadeer 1

---
+++ BootJMS02-ReceiverApp-pubsub/src/main/java/com/nt/BootJms01SubscriberAppApplication.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootJms01SubscriberAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootJms01SubscriberAppApplication.class, args);
+	}
+
+}
